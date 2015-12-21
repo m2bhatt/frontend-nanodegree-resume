9db8f79857bf4bf7f1f8cebbf3c09f2bc7498877
Megha Bhatt <meghabhatt15@gmail.com>
2015-12-21 16:39:43 -0500

    Recent changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-/* JSON Objects*/
+/* Bio Objects*/
 var bio = {
 	"name": "Megha Bhatt",
 	"role": "Web Developer",
@@ -7,116 +7,142 @@ var bio = {
 		"email": "meghabhatt15@gmail.com",
 		"github": "m2bhatt",
 		"twitter": "@m2bhatt",
-		"location": "Waterloo, ON, Canada"
-},
-	"welcomeMessage": "Welcome to my online resume",
-	"skills": ["HTML/CSS", "Leadership", "Quick learner", "Photoshop"],
-	"biopic": "images/fry.jpg"
+		"location": "Waterloo, Canada"
+	},
+	"biopic": "images/Megha.jpg",
+	"welcomeMessage": "I am a front end web developer with experience in data analysis, technical writing,marketing, communications, and social media marketing.",
+	"skills": ["HTML/CSS", "Leadership", "Quick learner", "Photoshop"]
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    $("#header").prepend(formattedName);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    $("#header").prepend(formattedRole);
-    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-    $("#header").append(formattedBioPic);
-    var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-    $("#header").append(formattedwelcomeMsg);
-    var formattedbioMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-    $("#topContacts").append(formattedbioMobile);
-    $("#footerContacts").append(formattedbioMobile);
-    var formattedbioEmail = HTMLemail.replace("%data%", bio.contacts.email);
-    var formattedbioEmail = formattedbioEmail.replace("#", bio.contacts.emailURL);
-    $("#topContacts").append(formattedbioEmail);
-    $("#footerContacts").append(formattedbioEmail);
-    var formattedbioGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-    var formattedbioGithub = formattedbioGithub.replace("#", bio.contacts.github);
-    $("#topContacts").append(formattedbioGithub);
-    $("#footerContacts").append(formattedbioGithub);
-    var formattedbioLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    var formattedbioLocation = formattedbioLocation.replace("#", bio.contacts.locationURL);
-    $("#topContacts").append(formattedbioLocation);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").prepend(formattedName);
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+	$("#header").append(formattedBioPic);
+	var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedwelcomeMsg);
+	var formattedbioMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedbioMobile);
+	$("#footerContacts").append(formattedbioMobile);
+	var formattedbioEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedbioEmail = formattedbioEmail.replace("#", bio.contacts.emailURL);
+	$("#topContacts").append(formattedbioEmail);
+	$("#footerContacts").append(formattedbioEmail);
+	var formattedbioGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedbioGithub = formattedbioGithub.replace("#", bio.contacts.github);
+	$("#topContacts").append(formattedbioGithub);
+	$("#footerContacts").append(formattedbioGithub);
+	var formattedbioTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	var formattedbioTwitter = formattedbioTwitter.replace("#", bio.contacts.twitter);
+	$("#topContacts").append(formattedbioTwitter);
+	$("#footerContacts").append(formattedbioTwitter);
+	var formattedbioLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	var formattedbioLocation = formattedbioLocation.replace("#", bio.contacts.locationURL);
+	$("#topContacts").append(formattedbioLocation);
+	$("#footerContacts").append(formattedbioLocation);
 
- if (bio.skills.length > 0) {
-        $("#header").append(HTMLskillsStart);
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-        $("#skills").append(formattedSkill);
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-        $("#skills").append(formattedSkill);
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-        $("#skills").append(formattedSkill);
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-        $("#skills").append(formattedSkill);
-    }
-
-var education = {
-	"schools": [
-	{
-		"name": "University of Waterloo",
-		"location": "Waterloo, ON, Canada",
-		"degree": "Bachelor of Arts",
-		"majors": ["Honours Rhetoric", "Professional Writing"],
-		"dates": 2014,
-		"url": "https://ugradcalendar.uwaterloo.ca/page/ARTS-Hon-English-Rhetoric-and-Professional-Writing"
-	},
-	{
-		"name": "University of Waterloo",
-		"location": "Waterloo, ON, Canada",
-		"degree": "Bachelor of Arts",
-		"majors": ["Honours Math", "Physics"],
-		"dates": 2014,
-		"url": "https://uwaterloo.ca/math/"
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+for(i in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
 	}
-	],
-	"onlineCourses": [
-	{
-		"title": "Intro to HTML and CSS",
-		"school": "Udacity",
-		"date": 2015,
-		"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
-	},
-		{
-		"title": "Javascript Basics",
-		"school": "Udacity",
-		"date": 2015,
-		"url": "https://www.udacity.com/course/javascript-basics--ud804"
-		}],
-	"display": "function"
-};
+}
+/* Work Experience */
 
 var work = {
 	"jobs": [{
 		"employer": "Shopify",
 		"title": "eCommerce Specialist",
-		"location": "Remoate in Ontario",
+		"location": "Ontario",
 		"dates": "October 2015 - Present",
 		"description": "I answer queries related to merchants' online businesses, inlcuding setting up stores, sales, billing, SEOs, online marketing, taxes, and domains."
 	},
 	{
 		"employer": "Canadian Touring Car Championship",
 		"title": "Communications and Social Media Manager",
-		"location": "Toronto, Ontario, Canada",
+		"location": "Toronto",
 		"dates": "March 2015 - Present",
 		"description": "I use social media to market CTCC's race events during the racing season. I interview drivers, collect and analyze vital information on the tracks and create a press release every race weekend."
-	}],
-};
-function displayWork() {
-for (job in work.jobs){
-	$("#workExperience").append(HTMLworkStart);
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-	$(".work-entry:last").append(formattedEmployerTitle);
-	
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDates);
-	
-	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	$(".work-entry:last").append(formattedDescription);
+	}]
 }
-};
+
+function displayWork(){
+	for (job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
+		
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+		
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+		}
+	}
+
 displayWork();
 
+/* Education */
+
+var education = {
+	"schools": [{
+		"name": "University of Waterloo",
+		"location": "Waterloo, ON, Canada",
+		"degree": "Bachelor of Arts",
+		"majors": ["Honours Rhetoric", "Professional Writing"],
+		"dates": 2014,
+		"url": "https://ugradcalendar.uwaterloo.ca/page/ARTS-Hon-English-Rhetoric-and-Professional-Writing"
+	},{
+		"name": "University of Waterloo",
+		"location": "Waterloo, ON, Canada",
+		"degree": "Bachelor of Arts",
+		"majors": ["Honours Math", "Physics"],
+		"dates": 2014,
+		"url": "https://uwaterloo.ca/math/"
+	}],
+	"onlineCourses": [{
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+	}, {
+		"title": "Javascript Basics",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/javascript-basics--ud804"
+		}]}
+
+education.display = function() {
+	$("#education").append(HTMLschoolStart);
+	for (school in education.schools){
+	var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+	var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+	var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].datesAttended);
+	var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+	var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+
+
+	$(".education-entry:last").append(formattedSchoolName);
+	$(".education-entry:last").append(formattedSchoolDegree);
+	$(".education-entry:last").append(formattedSchoolLocation);
+	$(".education-entry:last").append(formattedSchoolMajor);
+
+		$(".education-entry").append(HTMLonlineClasses);
+		for(course in education.onlineCourses){
+			var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourse[course].title);
+			var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+			var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+			var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+		}
+	}
+}
+education.display();
+
+/* Project */
 var projects = {
 	"projects":[{
 		"title":"Personal Website",
@@ -129,31 +155,53 @@ var projects = {
 		"dates":"February 2013",
 		"description":"I created an animated mug to learn more about animation via Blender.",
 		"images":["https://www.dropbox.com/s/wulzwlyolqxa431/Animated%20Mug.png?dl=0", "http://www.blender-models.com/wp-content/uploads/2013/09/kop-billede.jpg"]
-	}],
-	"display": "function"
+	}]}
+
+projects.display = function() {
+	for (projects in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+	var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+	var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+	var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+	var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+	var formattedDescriptionImage = formattedImage + formattedProjectDates + formattedProjectDescription + formattedTitle;
+
+		$("#projects").append(formattedDescriptionImage);
+	}
+}
+projects.display();
+
+
+projects.display = function(){
+	for (project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+		
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+		
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+		
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+		
+	if (projects.projects[project].images.length > 0){
+		for (image in projects.projects[project].images){
+			var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+			$(".project-entry:last").append(formattedImage);
+		}
+	}
+}
 };
 
 
+/* Maps */
+$("#mapDiv").append(googleMap);
 
 
-/* Displaying skills */
-if(bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-	$("#skills").append(formattedSkill);
-}
-
-$(document).click(function(loc){
-	var x = loc.pageX;
-	var y = loc.pageY;
-	
-	logClicks(x.y);
-});
+work.display();
+education.display();
+projects.display();
 
 
